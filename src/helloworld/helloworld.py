7a02f6838401b872bee92e5c99618ca5ec8da003
loganperreault <perreault.logan@gmail.com>
2014-06-26 08:59:45 -0600

    Test get message

--- src/helloworld/helloworld.py
+++ src/helloworld/helloworld.py
@@ -4,6 +4,7 @@ class MainPage(webapp2.RequestHandler):
     def get(self):
         self.response.headers['Content-Type'] = 'text/plain'
         self.response.write('Hello, World!')
+        self.response.out.write(cgi.escape(self.request.get('content')))
 
 application = webapp2.WSGIApplication([
     ('/', MainPage),
